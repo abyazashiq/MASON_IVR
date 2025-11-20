07b451aa9f94269e141f9b212e1a146c1e089d44
abyazashiq <abyazashiq@gmail.com>
2025-11-20 20:35:40 +0530

    front end building

--- backend/tester/database_test.py
+++ backend/tester/database_test.py
@@ -3,7 +3,7 @@ from database import insert_call
 # Insert a call
 insert_call(
     name="Alice Smith",
-    number="8888888888",
+    number="000000000",
     address="789 Lane",
     pay=1200,
     contact_status="Pending",
